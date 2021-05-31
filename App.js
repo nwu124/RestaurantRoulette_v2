c67cb78dd197fb8b9c095bf879161728cf56ced9
nwu124 <55820966+nwu124@users.noreply.github.com>
2021-05-31 13:59:07 -0700

    Update App.js

--- App.js
+++ App.js
@@ -93,6 +93,7 @@ var App = /** @class */ (function () {
         this.expressApp.use('/app/json/', express.static(__dirname + '/app/json'));
         this.expressApp.use('/images', express.static(__dirname + '/img'));
         this.expressApp.use('/', express.static(__dirname + '/pages'));
+        this.expressApp.use('/', express.static(__dirname + '/dist/app'));
     };
     return App;
 }());
